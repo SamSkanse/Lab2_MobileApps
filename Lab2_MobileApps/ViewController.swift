914e8dfbb0181760e9d2d1365b465104bf3288a8
SamSkanse <sskanse@smu.edu>
2024-10-07 23:42:08 -0500

    Initial Commit

---
+++ Lab2_MobileApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2_MobileApps
+//
+//  Created by Sam Skanse on 10/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lab2_MobileAppsTests/Lab2_MobileAppsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2_MobileAppsTests.swift
+//  Lab2_MobileAppsTests
+//
+//  Created by Sam Skanse on 10/7/24.
+//
+
+import Testing
+@testable import Lab2_MobileApps
+
+struct Lab2_MobileAppsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
