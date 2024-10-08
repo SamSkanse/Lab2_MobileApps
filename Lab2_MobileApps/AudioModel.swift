93f60fc0edeb2055fe0becacaba9fd31411fd446
Keaton-Harvey <123423711+Keaton-Harvey@users.noreply.github.com>
2024-10-08 17:51:30 -0500

    changes

--- Lab2_MobileApps/AudioModel.swift
+++ Lab2_MobileApps/AudioModel.swift
@@ -15,12 +15,10 @@ class AudioModel {
     
     // MARK: Properties
     private var BUFFER_SIZE:Int
+    // thse properties are for interfaceing with the API
+    // the user can access these arrays at any time and plot them if they like
     var timeData:[Float]
     var fftData:[Float]
-    var anotherData: [Float]  // New 20-point array property
-    private var volume:Float = 1.0
-    private var isPlaying = false
-    
     lazy var samplingRate:Int = {
         return Int(self.audioManager!.samplingRate)
     }()
@@ -28,43 +26,40 @@ class AudioModel {
     // MARK: Public Methods
     init(buffer_size:Int) {
         BUFFER_SIZE = buffer_size
+        // anything not lazily instatntiated should be allocated here
         timeData = Array.init(repeating: 0.0, count: BUFFER_SIZE)
         fftData = Array.init(repeating: 0.0, count: BUFFER_SIZE/2)
-        anotherData = Array(repeating: 0.0, count: 20)
     }
     
-    // public function for starting processing of the audio file
-    func startAudioFileProcessing(withFps: Double){
-        if let manager = self.audioManager, let fileReader = self.fileReader {
-            manager.outputBlock = self.handleSpeakerQueryWithAudioFile
+    // public function for starting processing of microphone data
+    func startMicrophoneProcessing(withFps:Double){
+        // setup the microphone to copy to circualr buffer
+        if let manager = self.audioManager{
+            manager.inputBlock = self.handleMicrophone
             
-            // Timer to process audio file data at specified FPS
+            // repeat this fps times per second using the timer class
+            //   every time this is called, we update the arrays "timeData" and "fftData"
             Timer.scheduledTimer(withTimeInterval: 1.0/withFps, repeats: true) { _ in
                 self.runEveryInterval()
             }
             
-            // Play the song through speakers
+        }
+    }
+    
+    
+    // You must call this when you want the audio to start being handled by our model
+    func play(){
+        if let manager = self.audioManager{
             manager.play()
-            isPlaying = true
         }
     }
     
-    func pause() {
-        if let manager = self.audioManager {
+    func pause(){
+        if let manager = self.audioManager{
             manager.pause()
-            isPlaying = false
         }
     }
     
-    private func calculateWindowedMaxima() {
-        let windowSize = fftData.count / 20
-        for i in 0..<20 {
-            let start = i * windowSize
-            let end = (i + 1) * windowSize
-            let window = fftData[start..<min(end, fftData.count)]
-            anotherData[i] = window.max() ?? 0.0  // Find max in the window
-        }
-    }
     
     //==========================================
     // MARK: Private Properties
@@ -76,37 +71,45 @@ class AudioModel {
         return FFTHelper.init(fftSize: Int32(BUFFER_SIZE))
     }()
     
-    private lazy var fileReader:AudioFileReader? = {
-        if let url = Bundle.main.url(forResource: "satisfaction", withExtension: "mp3"){
-            var tmpFileReader:AudioFileReader? = AudioFileReader.init(audioFileURL: url,
-                                                   samplingRate: Float(audioManager!.samplingRate),
-                                                   numChannels: audioManager!.numOutputChannels)
-            tmpFileReader!.currentTime = 0.0 // start from time zero!
-            print("Audio file succesfully loaded for \(url)")
-            return tmpFileReader
-        } else {
-            print("Could not initialize audio input file")
-            return nil
-        }
+    
+    private lazy var inputBuffer:CircularBuffer? = {
+        return CircularBuffer.init(numChannels: Int64(self.audioManager!.numInputChannels),
+                                   andBufferSize: Int64(BUFFER_SIZE))
     }()
     
+    
+    //==========================================
+    // MARK: Private Methods
+    // NONE for this model
+    
+    //==========================================
+    // MARK: Model Callback Methods
     private func runEveryInterval(){
-        if let file = self.fileReader {
-            // Get fresh audio data from file
-            file.retrieveFreshAudio(&timeData, numFrames: UInt32(BUFFER_SIZE), numChannels: 1)
+        if inputBuffer != nil {
+            // copy time data to swift array
+            self.inputBuffer!.fetchFreshData(&timeData, // copied into this array
+                                             withNumSamples: Int64(BUFFER_SIZE))
             
-            // Perform FFT on the audio data
+            // now take FFT
             fftHelper!.performForwardFFT(withData: &timeData,
-                                         andCopydBMagnitudeToBuffer: &fftData)
-            calculateWindowedMaxima()
+                                         andCopydBMagnitudeToBuffer: &fftData) // fft result is copied into fftData array
+            
+            // at this point, we have saved the data to the arrays:
+            //   timeData: the raw audio samples
+            //   fftData:  the FFT of those same samples
+            // the user can now use these variables however they like
+            
         }
     }
     
-    private func handleSpeakerQueryWithAudioFile(data: Optional<UnsafeMutablePointer<Float>>, numFrames: UInt32, numChannels: UInt32){
-        if let file = self.fileReader, let arrayData = data {
-            // Get samples from audio file, adjust volume
-            file.retrieveFreshAudio(arrayData, numFrames: numFrames, numChannels: numChannels)
-            vDSP_vsmul(arrayData, 1, &(self.volume), arrayData, 1, vDSP_Length(numFrames * numChannels))
-        }
+    //==========================================
+    // MARK: Audiocard Callbacks
+    // in obj-C it was (^InputBlock)(float *data, UInt32 numFrames, UInt32 numChannels)
+    // and in swift this translates to:
+    private func handleMicrophone (data:Optional<UnsafeMutablePointer<Float>>, numFrames:UInt32, numChannels: UInt32) {
+        // copy samples from the microphone into circular buffer
+        self.inputBuffer?.addNewFloatData(data, withNumSamples: Int64(numFrames))
     }
+    
+    
 }
--- Lab2_MobileApps/ModuleBViewController.swift
+++ Lab2_MobileApps/ModuleBViewController.swift
@@ -8,8 +8,24 @@
 import UIKit
 
 class ModuleBViewController: UIViewController {
+    
+    struct AudioConstants{
+            static let AUDIO_BUFFER_SIZE = 1024*4
+        }
+    
+    let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        audio.play()
     }
-}
+    
+    override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            
+            // Stop audio input when leaving view
+        audio.pause()
+        }
+    
+}//end of class
